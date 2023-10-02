2758f7455d6238769b45579765e19b4b37badbc0
stefanusvemas <stefanusvemas@gmail.com>
2023-10-02 15:59:56 +0700

    first commit

---
+++ resources/views/Login.blade.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="e n">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body class="antialiased">
+    <style>
+        body {
+            font-family: Roboto, Helvetica, Arial, sans-serif;
+        }
+
+        section {
+            width: 100%;
+            min-height: 100vh;
+        }
+
+        .background-radial-gradient {
+            background-color: hsl(218, 41%, 15%);
+            background-image: radial-gradient(650px circle at 0% 0%,
+                    hsl(218, 41%, 35%) 15%,
+                    hsl(218, 41%, 30%) 35%,
+                    hsl(218, 41%, 20%) 75%,
+                    hsl(218, 41%, 19%) 80%,
+                    transparent 100%),
+                radial-gradient(1250px circle at 100% 100%,
+                    hsl(218, 41%, 45%) 15%,
+                    hsl(218, 41%, 30%) 35%,
+                    hsl(218, 41%, 20%) 75%,
+                    hsl(218, 41%, 19%) 80%,
+                    transparent 100%);
+        }
+
+        #radius-shape-1 {
+            height: 220px;
+            width: 220px;
+            top: -60px;
+            left: -130px;
+            background: radial-gradient(#44006b, #ad1fff);
+            overflow: hidden;
+        }
+
+        #radius-shape-2 {
+            border-radius: 38% 62% 63% 37% / 70% 33% 67% 30%;
+            bottom: -60px;
+            right: -110px;
+            width: 300px;
+            height: 300px;
+            background: radial-gradient(#44006b, #ad1fff);
+            overflow: hidden;
+        }
+
+        .bg-glass {
+            background-color: hsla(0, 11%, 95%, 0.9) !important;
+            backdrop-filter: saturate(200%) blur(25px);
+        }
+    </style>
+
+    <section class="background-radial-gradient overflow-hidden">
+        <div class="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
+            <div class="row gx-lg-5 align-items-center mb-5">
+                <div class="col-lg-6 mb-5 mb-lg-0" style="z-index: 10">
+                    <h1 class="my-5 display-5 fw-bold ls-tight" style="color: hsl(218, 81%, 95%)"> Gofit Indonesia<br />
+                        <span style="color: hsl(218, 81%, 75%)">Make Your Body Perfect</span>
+                    </h1>
+                    <p class="mb-4 opacity-70 justify-content-center" style="color: hsl(218, 81%, 85%); text-align: justify">
+                        Are you tired of feeling sluggish and out of shape? Do you want to improve your health and fitness level? Look no further than your local gym!
+                    </p>
+                    <p class="mb-4 opacity-70 " style="color: hsl(218, 81%, 85%); text-align: justify;">
+                        At the gym, you'll have access to state-of-the-art equipment and a variety of fitness classes to help you reach your fitness goals. Whether you're looking to build muscle, lose weight, or simply improve your overall health, our gym has everything you need to succeed. Our experienced and knowledgeable trainers are on hand to provide guidance and support every step of the way. They can help you develop a personalized workout plan that's tailored to your specific needs and goals, and they'll be there to encourage and motivate you to push yourself to new heights. And with a community of like-minded individuals, you'll have the support and
+                        encouragement you need to stay motivated and committed to your fitness journey. You'll make new friends, have fun, and most importantly, see results.
+                    </p>
+                </div>
+                <div class="col-lg-6 mb-5 mb-lg-0 position-relative">
+                    <div id="radius-shape-1" class="position-absolute rounded-circle shadow-5-strong"></div>
+                    <div id="radius-shape-2" class="position-absolute shadow-5-strong"></div>
+                    <div class="card bg-glass">
+                        <div class="card-body px-4 py-5 px-md-5">
+                            <form class="form" action="{{ url('gyms') }}"> @csrf
+                                <div>
+                                    <h4 class="mb-3 text-center">LOGIN</h4>
+                                </div>
+                                <div class="form-floating mb-2">
+                                    <input type="text" class="form-control" id="floatingInput" placeholder="Nama Pegawai" required />
+                                    <label for="floatingInput">Nama Pegawai</label>
+                                </div>
+                                <!-- Password -->
+                                <div class="form-floating">
+                                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password" required />
+                                    <label for="floatingPassword">Password</label>
+                                </div>
+                                <!-- Submit button -->
+                                <button type="submit" style="width: 100%;" class="btn btn-primary btn-block mb-2 mt-3 "> Login
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('Login');
+});
+Route::get('/dashboard', function () {
+    return view('Dashboard');
+});
+Route::get('/gyms', function () {
+    return view('Gyms/index', [
+        'kelas' => [
+            [
+                'no' => 1,
+                "gambar" => "https://www.fitnessfirst.co.id/id/-/media/project/evolution-wellness/fitness-first/south-eastasia/indonesia/classes/bodycombat/bodycombat_fb-sharing.png",
+                "nama" => "Body Combat",
+                'instruktur' => 'Jolly',
+                'ruang' => 'Kelas A',
+                'rating' => '5'
+            ],
+            [
+                'no' => 2,
+                'gambar' => 'https://media.tacdn.com/media/attractions-splicespp-674x446/06/dc/83/bc.jpg',
+                'nama' => 'Bungee ',
+                'instruktur' => 'Agung',
+                'ruang' => 'Kelas B',
+                'rating' => '3',
+            ],
+            [
+                'no' => 3,
+                'gambar' => 'https://fitbod.me/wpcontent/uploads/2021/07/yoga-and-the-gym-on-the-same-day.jpg',
+                'nama' => 'Yogalates ',
+                'instruktur' => 'Raka',
+                'ruang' => 'Kelas C',
+                'rating' => '4',
+            ],
+            [
+                'no' => 4,
+                'gambar' =>
+                'https://res.cloudinary.com/display97/image/upload/7930/boxing2-224119.jpeg',
+                'nama' => 'Boxing ',
+                'instruktur' => 'Tebri',
+                'ruang' => 'Kelas D',
+                'rating' => '5',
+            ]
+        ]
+    ]);
+});
